0eb52ad711ac043cceff6c49429be7ac33fec4c3
manasab660-blip <manasab660@gmail.com>
2025-11-28 15:04:35 +0530

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,33 +1,21 @@
 <?php
-$fullname = $_POST['fullname'];
+$name = $_POST['name'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
-$dob = $_POST['dob'];
-$gender = $_POST['gender'];
-$address = $_POST['address'];
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
     <title>Application Submitted</title>
     <link rel="stylesheet" href="style.css">
 </head>
-
 <body>
 
-<h1>Application Submitted Successfully</h1>
+<h2>Application Received Successfully</h2>
 
-<div style="background:#fff; padding:20px; width:400px; border-radius:10px; box-shadow:0px 0px 10px #999;">
-
-    <p><strong>Full Name:</strong> <?php echo $fullname; ?></p>
-    <p><strong>Email:</strong> <?php echo $email; ?></p>
-    <p><strong>Phone:</strong> <?php echo $phone; ?></p>
-    <p><strong>Date of Birth:</strong> <?php echo $dob; ?></p>
-    <p><strong>Gender:</strong> <?php echo $gender; ?></p>
-    <p><strong>Address:</strong> <?php echo nl2br($address); ?></p>
-
-</div>
+<p><strong>Name:</strong> <?php echo $name; ?></p>
+<p><strong>Email:</strong> <?php echo $email; ?></p>
+<p><strong>Phone:</strong> <?php echo $phone; ?></p>
 
 </body>
 </html>
